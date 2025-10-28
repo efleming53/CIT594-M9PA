f8ddc4566b34b6040e9f2877facc5605c2118a6a
Eric Fleming <eflem53@seas.upenn.edu>
2025-10-28 17:55:39 -0400

    Complete, but fixing autograder tests issue

--- CIT594-M9PA/src/KeywordGraph.java
+++ CIT594-M9PA/src/KeywordGraph.java
@@ -7,9 +7,9 @@
  * university code of academic integrity:
  *  https://catalog.upenn.edu/pennbook/code-of-academic-integrity/ >
  * Signed,
- * Author: YOUR NAME HERE
- * Penn email: <YOUR-EMAIL-HERE@seas.upenn.edu>
- * Date: YYYY-MM-DD
+ * Author: Eric Fleming
+ * Penn email: eflem53@seas.upenn.edu
+ * Date: 2025-10-23
  */
 
 import java.util.*;
@@ -27,29 +27,151 @@ public class KeywordGraph {
     // build a graph that models keyword co-occurrence relationships from tech news articles.
     public void buildGraphFromArticles(List<Article> articles) {
         //TODO: follow instructions to complete this method
+    	
+    	// loop over articles
+    	for (Article article : articles) {
+    		
+    		List<Concept> concepts = article.getConcepts(); // get concepts for current article
+    		
+    		// skip empty or invalid lists of concepts
+    		if (concepts == null || concepts.size() < 2) {
+    			continue;
+    		}
+    		
+    	
+    		List<String> keywords = new ArrayList<>(); // list to hold keywords for this article
+    		// loop over articles concepts
+    		for (Concept concept : concepts) {
+    			keywords.add(concept.getLabel());
+    		}
+    		
+    		// loop over articles keywords
+    		for (String keyword1 : keywords) {
+    			adjList.putIfAbsent(keyword1, new TreeSet<>()); // put keyword in graphs adjList if not already present, value is treeset to hold neighbor keywords
+    			
+    			// nested loop to add edges both ways
+    			for (String keyword2 : keywords) {
+    				
+    				// skip if same word as outer loop
+    				if (!keyword2.equals(keyword1)) {
+    					adjList.putIfAbsent(keyword2, new TreeSet<>()); // put keyword if not already in adjList
+    					adjList.get(keyword1).add(keyword2); // ensures every keyword in this aritcle has every other keyword included in its set of neighbors
+    					adjList.get(keyword2).add(keyword1);
+    				}
+    			}
+    		}
+    	}
     }
 
 
     // perform Breadth-First Search to explore related keywords in lexicographic order.
     public List<String> bfs(String rawKeyword) {
         //TODO: follow instructions to complete this method
+    	
+    	String word = rawKeyword.toLowerCase().trim(); // process raw keyword
+    	
+    	// if word not in adjList, return empty list
+    	if (!adjList.containsKey(word)) {
+    		return new ArrayList<>();
+    	}
+    	
+    	Set<String> visited = new HashSet<>(); // set to hold visited nodes
+    	Queue<String> visiting = new LinkedList<>(); // queue to hold nodes in order they will be processed
+    	
+    	visited.add(word); // add given word to visited set
+    	visiting.add(word); // add given word to visiting queue
+    	
+    	// loop until queue of nodes to visit is empty
+    	while (!visiting.isEmpty()) {
+    		
+    		String currWord = visiting.remove(); // get next node to process from queue
+    		
+    		// for each neighbor of the word currently being processed
+    		for (String neighbor : adjList.get(currWord)) {
+    			
+    			// if the neighbor is not in visited, add to visited AND visiting
+    			if (!visited.contains(neighbor)) {
+    				visited.add(neighbor);
+    				visiting.add(neighbor);
+    			}
+    		}
+    	}
 
-        return null;
+        return new ArrayList<>(visited); // return visited set as new ArrayList
     }
 
 
     // perform Depth-First Search to explore keyword chains in lexicographic order.
     public List<String> dfs(String rawKeyword) {
         //TODO: follow instructions to complete this method
+    	
+    	String word = rawKeyword.toLowerCase().trim(); // process given word
+    	// return empty list if word not found
+    	if (!adjList.containsKey(word)) {
+    		return new ArrayList<>();
+    	}
+    	
+    	Set<String> visited = new HashSet<>(); // create set to hold visited nodes
+    	
+    	doDFS(word, visited); // call recursive function to do DFS
 
-        return null;
+        return new ArrayList<>(visited); // return set of visited nodes as new ArrayList
+    }
+    
+    private Set<String> doDFS(String currWord, Set<String> visited) {
+    	
+    	// base case = currWord is already in visited, return
+    	if (visited.contains(currWord)) {
+    		return visited;
+    	}
+    	
+    	visited.add(currWord); // add word to visited set
+    	
+    	// loop over words neighbors, if not in visited, recursively call this function
+    	for (String neighbor : adjList.get(currWord)) {
+    		if (!visited.contains(neighbor)) {
+    			doDFS(neighbor, visited);
+    		}
+    	}  	
+    	return visited; // return visited set
     }
 
 
     // returns top-K most connected keywords based on degree.
     public List<String> topKDegree(int k) {
         //TODO: follow instructions to complete this method
+    	
+    	// return empty list if adjList empty or invalid k
+    	if (adjList.isEmpty() || k <= 0) {
+    		return new ArrayList<>();
+    	}
+    	
+    	List<String> words = new ArrayList<>(adjList.keySet()); // create list will all words
+    	
+    	// sort words using Comparator function
+    	words.sort((String w1, String w2) -> {
+    		
+    		// get degrees of words being compared (size of neighbor set)
+    		int w1Degree = adjList.get(w1).size();
+    		int w2Degree = adjList.get(w2).size();
+    		
+    		if (w1Degree > w2Degree) {return -1;} // if w1 has higher degree
+    		if (w1Degree < w2Degree) {return 1;} // if w2 has higher degree
+    		else {return w1.compareTo(w2);} // if words have same degree, make comparison based on lexicographical order
+    	});
+    	
+    	List<String> topKWords = new ArrayList<>();
+    	
+    	// adjust k if bigger than number of words
+    	if (k > words.size()) {
+    		k = words.size();
+    	}
+    	
+    	// add all top k words to list to be returned
+    	for (int i = 0; i < k; i++) {
+    		topKWords.add(words.get(i));
+    	}
 
-        return null;
+        return topKWords;
     }
 }
--- CIT594-M9PA/src/KeywordGraphTest.java
+++ CIT594-M9PA/src/KeywordGraphTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
 
 public class KeywordGraphTest {
 
@@ -8,18 +9,92 @@ public class KeywordGraphTest {
 
     @BeforeAll
     public static void setup() {
-        // uncomment the lines below and provide correct path to your dataset file.
-        // List<Article> articles = ArticleLoader.loadFromCSV("m9pa_dataset_simplified.csv");
-        // graph = new KeywordGraph();
-        // graph.buildGraphFromArticles(articles);
     }
 
     @Test
     public void testKeywordGraph() {
-        // TODO: write at least 3 test cases for each method to test functionality
+    	
+    	// load articles
+        List<Article> articles = ArticleLoader.loadFromCSV("m9pa_dataset_simplified.csv");
+        graph = new KeywordGraph();
+        graph.buildGraphFromArticles(articles);
+        System.out.println("Articles loaded: " + articles.size());
 
-        // example
-        // List<String> top = graph.topKDegree(1);
-        // assertFalse(top.isEmpty(), "Graph should contain nodes.");
+        // test graph not empty
+        List<String> top1 = graph.topKDegree(1);
+        assertFalse(top1.isEmpty(), "Graph should contain at least one node.");
+
+        // test contains first keyword from first article
+        List<String> allNodes = graph.topKDegree(100); // get many nodes
+        String firstArticleKeyword = articles.get(0).getConcepts().get(0).getLabel();
+        assertTrue(allNodes.contains(firstArticleKeyword), "Graph should contain first keyword");
+        
+        // test contains first keyword from thirtieth article
+        String thirtiethArticleKeyword = articles.get(29).getConcepts().get(0).getLabel();  
+        assertTrue(allNodes.contains(thirtiethArticleKeyword), "Graph should contain fiftieth keyword");
+    }
+    
+    @Test
+    public void testBFS() {
+        List<Article> articles = ArticleLoader.loadFromCSV("src/m9pa_dataset_simplified.csv");
+        graph = new KeywordGraph();
+        graph.buildGraphFromArticles(articles);
+
+        // test BFS return contains first node
+        String firstNode = articles.get(0).getConcepts().get(0).getLabel();
+        List<String> bfsResult = graph.bfs(firstNode);
+        assertTrue(bfsResult.contains(firstNode), "BFS result should include first node.");
+
+        // test BFS returns labels for all concepts in same article
+        List<Concept> firstArticleConcepts = articles.get(0).getConcepts();
+        for (Concept concept : firstArticleConcepts) {
+                assertTrue(bfsResult.contains(concept.getLabel()), "BFS should reach all concepts in the same article.");
+        }
+
+        List<String> bfsNonExistent = graph.bfs("doesnt_exist");
+        assertTrue(bfsNonExistent.isEmpty(), "BFS on non-existent word should return an empty list.");
+    }
+    
+    @Test
+    public void testDFS() {
+        List<Article> articles = ArticleLoader.loadFromCSV("src/m9pa_dataset_simplified.csv");
+        graph = new KeywordGraph();
+        graph.buildGraphFromArticles(articles);
+
+        // test DFS return contains first node
+        String firstNode = articles.get(0).getConcepts().get(0).getLabel();
+        List<String> dfsResult = graph.dfs(firstNode);
+        assertTrue(dfsResult.contains(firstNode), "DFS result should include first node.");
+
+        // test DFS returns labels for all concepts in same article
+        List<Concept> firstArticleConcepts = articles.get(0).getConcepts();
+        for (Concept concept : firstArticleConcepts) {
+                assertTrue(dfsResult.contains(concept.getLabel()), "DFS should reach all concepts in the same article.");
+        }
+
+        // test non-existent word in dfs
+        List<String> dfsNonExistent = graph.dfs("doesnt_exist");
+        assertTrue(dfsNonExistent.isEmpty(), "dFS on non-existent word should return an empty list.");
+    }
+    
+    @Test
+    public void testTopKDegree() {
+    	 List<Article> articles = ArticleLoader.loadFromCSV("src/m9pa_dataset_simplified.csv");
+    	 graph = new KeywordGraph();
+    	 graph.buildGraphFromArticles(articles);
+
+    	    // test top 1 node is returned
+    	    List<String> top1 = graph.topKDegree(1);
+    	    assertFalse(top1.isEmpty(), "topKDegree(1) should return at least one node.");
+
+    	    // test top 5 nodes are returned
+    	    List<String> top5 = graph.topKDegree(5);
+    	    assertEquals(5, top5.size(), "topKDegree(5) should return 5 nodes.");
+
+    	    // test all nodes returned when k > number of nodes
+    	    List<String> allNodes = graph.topKDegree(1000);
+    	    List<String> graphNodes = graph.topKDegree(graph.topKDegree(1000).size());
+    	    assertEquals(allNodes.size(), graphNodes.size(), "Requesting more nodes than exist should return all nodes.");
+
     }
 }
